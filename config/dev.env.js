6fbe27617840678c913f58fca4bd9b397e4986b3
Hugh <1214219989@qq.com>
2020-11-30 01:04:16 +0800

    hello2

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
-  BASE_API: '"http://localhost:9090"',  //使用nginx进行请求转发
+  BASE_API: '"http://localhost:8888"'
 })
